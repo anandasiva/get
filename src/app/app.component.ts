523b71083bba3571da9e3a3586a33ebb510b08cd
anandasiva <anandasiva4@gmail.com>
2023-05-03 05:51:09 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class Repos{
+  id!:string;
+  name!: string;
+  html_url!: string;
+  description!: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'get';
+  userName:string="tektutorialshub"
+  baseURL:string="https://api.github.com/";
+  repos!: Repos[];
+
+constructor(private http:HttpClient){
+
+}
+ngOnInit(){
+   this.getRepos()
+}
+
+public getRepos(){
+
+  return this.http.get<Repos[]>(this.baseURL + 'users/'+ this.userName +'/repos')
+  .subscribe(
+   (response) =>{
+    console.log('response received')
+    console.log(response);
+    this.repos = response;
+   },
+   
+   (error) =>{
+    console.error('Request failed with error')
+    alert(error);
+   },
+   ()=>{
+    console.log('Request completed')
+   })
+
+}
+
 }
